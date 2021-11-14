f7766ba474386baa247b20a7042ac2cba1ca25e1
Govardhan8 <govardhanmurthy007@gmail.com>
2021-11-14 13:23:54 +0530

    footer content

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,7 @@
 import './index.css'
 
 const index = () => {
-	return <footer>Copyright © Your Website 2021</footer>
+	return <footer>Copyright © Let's Shop 2021</footer>
 }
 
 export default index
